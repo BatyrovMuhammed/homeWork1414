003dbdfaa0ae6a212471b3d9177cdcf97d8cb458
ergeshmametuulukanat <ergeshmametuulukanat@gmail.com>
2021-03-30 23:51:19 +0600

    commit one

---
+++ src/main/java/peaksoft/config/AppConfig.java
@@ -0,0 +1,12 @@
+package peaksoft.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan(basePackages = "peaksoft")
+public class AppConfig {
+
+}
